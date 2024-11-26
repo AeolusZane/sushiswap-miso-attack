3f8e6efe3aa25c7aed61ee16e98f0896f9324561
AeolusZane <1393649219@qq.com>
2024-11-27 00:09:48 +0800

    delete test

--- test.js
+++
@@ -1,7 +0,0 @@
-// const hre = require("hardhat");
-// const msgHash = hre.ethers.hashMessage;
-// const hexlify = hre.ethers.hexlify;
-// const getBytes = hre.ethers.getBytes;
-// console.log(msgHash("Hello, "))
-// console.log(hexlify(getBytes('0xaeb29eeb84bfb2bd44ac9772603611041bf39a774d9950afc45d2f9b')));
-// // console.log(hre.ethers.concat([hexlify(getBytes("Hello, ")),msgHash("Hello, "),msgHash("Hello, ")]));
